36e0c4551bbefe3641c63482ad89cbe593b08ce4
Dominik Clemenete <dominik.clemente@gmail.com>
2020-05-22 13:50:15 +0100

    Updates

---
+++ precache-manifest.ad3ea98d58665d64a250ab94c310857f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9f17e7a4163c1990faefaa53b67634",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c5123b1c1f2ee33891a",
+    "url": "/monsters-rolodex/static/css/main.28401bcb.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5123b1c1f2ee33891a",
+    "url": "/monsters-rolodex/static/js/main.e237c60d.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
